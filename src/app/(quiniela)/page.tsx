1ed43a300d7500adb848edf5f28e697950c5bb79
vcontreras <contrerastrece@gmail.com>
2024-05-09 07:45:35 -0500

    eliminando bug

--- src/app/(quiniela)/page.tsx
+++ src/app/(quiniela)/page.tsx
@@ -15,12 +15,6 @@ export default async function HomePage() {
   return (
     <div className="text-white">
       <section className="px-2 md:px-4 md:w-[600px]">
-        <div className="flex justify-between items-center mb-4 md:mb-2">
-          {/* <h1 className="text-md md:text-xl font-bold">MATCHES</h1> */}
-          <div className="px-4 py-0 md:py-1 bg-slate-600 rounded-md text-textPrimary text-sm">
-            {/* <p>{`${dateConvert}`}</p> */}
-          </div>
-        </div>
         <Status user={user} />
       </section>
     </div>

e0eefa0b7cd9c261e0db748651a4c5db4641ec2f
vcontreras <contrerastrece@gmail.com>
2024-05-14 18:02:22 -0500

    cambiar consulta de api para la competiciones

--- src/app/(quiniela)/competitions/page.tsx
+++ src/app/(quiniela)/competitions/page.tsx
@@ -2,11 +2,11 @@ import { CardCompetition, CardDefault } from "@/components";
 import React from "react";
 
 const HomePage = async () => {
-  const response = await fetch("http://localhost:3000/competitions/api");
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/competitions/api`);
   const data = await response.json();
   return (
-    <div className="grid grid-cols-2 gap-4 p-2">
-      <CardDefault/>
+    <div className="grid grid-cols-2 gap-4 p-2  max-w-2xl mx-auto">
+      <CardDefault />
       {data.competitions.map((c: any) => (
         <CardCompetition data={c} key={c.id} />
       ))}

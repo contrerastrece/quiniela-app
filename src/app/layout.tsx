c7339d49df35fcc90a7cd3a664473acb913083f6
vcontreras <contrerastrece@gmail.com>
2024-05-14 19:48:14 -0500

    agregando popcash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+      <meta name="ppck-ver" content="e53f623e73177d465a4d4d40810480b6" />
+      </head>
       <body className={`${inter.className} bg-slate-900`}>
         <main className="">
           <TansTackProvider>{children}</TansTackProvider>
